979194ae5b941214312e0e7ec00a5d7b19530e2d
vingorius <vingorius@gmail.com>
2015-11-24 17:47:47 +0900

    add many function

--- public/javascripts/clientui.js
+++ public/javascripts/clientui.js
@@ -1,7 +1,12 @@
 $(document).ready(function() {
-
-    var socket = io();
-    var user = {};
+    var host = location.protocol + '//' + location.hostname + (location.port ? ':' + location.port : '');
+    host += '/tie';
+    console.log('client nsp:%s', host);
+    var socket = io(host);
+    var user = {
+        name: null,
+        rooms: []
+    };
     var cur_room_name;
 
     function findRoom(room_name) {
@@ -12,6 +17,12 @@ $(document).ready(function() {
             }
         }
     }
+    findRoomByFriend = function(friend_name) {
+        for (var i = 0; i < user.rooms.length; i++) {
+            if (user.rooms[i].users.indexOf(friend_name) > -1) return user.rooms[i];
+        }
+        return null;
+    };
 
     function renderRoom(room) {
         $('#rooms').append('<li id="' + room.name + '" class="list-group-item">' + room.name + ' <span class="badge">' + room.talks.length + '</span></li>');
@@ -36,7 +47,16 @@ $(document).ready(function() {
         room_badge.text(Number(room_badge.text()) + 1);
     }
 
+    function clear(){
+        //clear before start.
+        $('#friends').empty();
+        $('#rooms').empty();
+    }
+
     socket.on('auth', function(p_user) {
+        //clear before start.
+        clear();
+
         user = p_user;
         console.log('auth', user);
         user.friends.forEach(function(friend) {
@@ -70,19 +90,31 @@ $(document).ready(function() {
     });
 
     $('#authbtn').click(function() {
-        $('#friends').empty();
-        $('#rooms').empty();
-
         user.name = $('#id').val();
         socket.emit('auth', user);
     });
 
-    $('#friends').on('click', 'li', function() {
-        var friend = $(this).text();
-        $('#friend').addClass('label-primary').text(friend);
-        socket.emit('room', friend, function(room) {
-            renderRoom(room);
+    $('#exitbtn').click(function() {
+        socket.emit('exit', function() {
+            console.log('exit success.');
+            clear();
         });
+    });
+
+    $('#friends').on('click', 'li', function() {
+        var friend_name = $(this).text();
+        $('#friend').addClass('label-primary').text(friend_name);
+
+        var room = findRoomByFriend(friend_name);
+        if (!room) {
+            socket.emit('room', friend_name, function(room) {
+                user.rooms.push(room);
+                renderRoom(room);
+            });
+        } else {
+            console.log('room %s is already exist.', room.name);
+            $("#rooms li:contains('" + room.name + "')").trigger('click');
+        }
     });
 
     $('#rooms').on('click', 'li', function() {
@@ -93,6 +125,7 @@ $(document).ready(function() {
     });
 
     $('#roomModal').on('show.bs.modal', function(event) {
+        $('#myModalLabel').text(cur_room_name);
         var room = findRoom(cur_room_name);
         room.talks.forEach(function(talk) {
             renderMessage(talk);
@@ -121,4 +154,38 @@ $(document).ready(function() {
         socket.emit('talk', room.name, talk);
         $('#m').val('');
     });
+
+    // Debugging Listener
+    socket.on('connect', function() {
+        console.log('Socket[%s] is connected.', socket.id);
+    });
+
+    socket.on('connect_error', function(err) {
+        console.log('Socket[%s] is connect_error:%s', socket.id, err);
+    });
+
+    socket.on('reconnect', function(no) {
+        console.log('Socket[%s] reconnected after trying %d times', socket.id, no);
+        user = {};
+        user.name = $('#id').val();
+        console.log('i will auth automatically with %s.', user.name);
+        socket.emit('auth', user);
+    });
+
+    socket.on('reconnect_attempt', function() {
+        console.log('Socket[%s] is reconnect_attempt.', socket.id);
+    });
+
+    socket.on('reconnecting', function(no) {
+        console.log('Socket[%s]  is reconnecting %d times.', socket.id, no);
+    });
+
+    socket.on('reconnect_error', function(err) {
+        console.log('Socket[%s] is reconnect_error:%s', socket.id, err);
+    });
+
+    socket.on('reconnect_failed', function() {
+        console.log('Socket[%s] is reconnect_failed.', socket.id);
+    });
+
 });
--- tie.js
+++ tie.js
@@ -7,36 +7,53 @@ var Talk = require('./lib/talk');
 
 function init() {
     console.log('init');
-    
+
     var room1 = roomdb.createRoom(['vingorius', 'fish']);
     var room2 = roomdb.createRoom(['vingorius', 'ms']);
 
     var usr1 = userdb.createUser('vingorius', ['fish', 'ms', 'mk', 'mh'], [room1, room2]);
-    var usr2 = userdb.createUser('fish', ['vingorius', 'ms', 'mk', 'mh'], [room1]);
-    var usr3 = userdb.createUser('ms', ['vingorius', 'fish', 'mk', 'mh']);
+    var usr2 = userdb.createUser('fish', ['vingorius', 'ms'], [room1]);
+    var usr3 = userdb.createUser('ms', ['vingorius', 'fish'], [room2]);
     var usr4 = userdb.createUser('mk', ['vingorius']);
+    var usr5 = userdb.createUser('mh', ['vingorius']);
 }
 
 function sendErr(socket, msg) {
     //TODO err 보내는 것. 나한테만 보내야한다.
-    socket.to(socket.id).emit('err', msg);
+    console.log('send %s to %s ', msg, socket.id);
+    socket.emit('err', msg);
+}
+
+function showSocketInfo(socket) {
+    console.log('------------Socket Info:%s--------------', socket.id);
+    console.log('rooms:%s', socket.rooms);
+    if (socket.user) {
+        console.log('user:', socket.user.name);
+    }
+    console.log('-----------------------------------------');
 }
 
 exports.createServer = function(http) {
     var io = new Server(http);
+    var nsp = io.of('/tie');
     init();
 
-    // io.use(function(socket, next) {
-    //     console.log('io.use',socket);
-    //     next();
-    // });
+    console.log('path', io.path());
+    console.log('origins', io.origins());
 
-    io.on('connection', function(socket) {
+    //새로운 Client 소켓이 만들어질 때마다 호출된다.
+    nsp.use(function(socket, next) {
+        console.log('New Socket %s is created.', socket.id);
+        next();
+    });
+
+    nsp.on('connection', function(socket) {
         socket.auth = false;
         socket.user = null;
+        console.log('New Socket %s is connected.', socket.id);
 
         socket.on('auth', function(p_user) {
-            console.log('auth', p_user);
+            console.log('%s is auth by user[%s]', socket.id, p_user.name);
             userdb.auth(p_user, function(user) {
                 if (user) {
                     user.setSocketId(socket.id);
@@ -50,29 +67,34 @@ exports.createServer = function(http) {
             });
         });
 
+        /**
+         * create new room.
+         * 혹 room 이 이미 있는 경우를 위해 체크하는 로직 포함.
+         * if not login, just add the room on user.
+         * if login, after add, emit 'room' event.
+         */
         socket.on('room', function(friend_name, cb) {
             if (!socket.auth) return sendErr(socket, 'not auth');
 
-            var room = roomdb.createRoom([socket.user.name, friend_name],new Talk(socket.user.name, 'new room'));
-            socket.user.addRoom(room);
-
-            socket.join(room.name);
-
             var friend = userdb.findUser(friend_name);
-            console.log('room,friend', friend);
-            if (friend) {
+            if (!friend) return sendErr(socket, 'no friend');
+            var room = socket.user.findRoom(friend_name);
+            if (!room) {
+                console.log('room: create new room with %s', friend_name);
+                room = roomdb.createRoom([socket.user.name, friend_name], new Talk(socket.user.name, 'new room'));
+                socket.user.addRoom(room);
+                socket.join(room.name);
                 friend.addRoom(room);
                 if (friend.isLogin()) {
-                    socket.broadcast.to(friend.getSocketId()).emit('room', room);
+                    nsp.to(friend.getSocketId()).emit('room', room);
                 }
-                cb(room);
-            } else {
-                sendErr(socket, 'no friend');
             }
+            cb(room);
         });
 
+
         /**
-         * room 콜을 받은 client가 join하기 위해 날린다.
+         * room 콜을 받은 client가 room에 join하기 위해 날린다.
          */
         socket.on('join', function(room_name) {
             if (!socket.auth) return sendErr(socket, 'not auth');
@@ -89,23 +111,28 @@ exports.createServer = function(http) {
 
         socket.on('talk', function(room_name, talk) {
             if (!socket.auth) return sendErr(socket, 'not auth');
+            // showSocketInfo(socket);
 
+            console.log('%s send "%s" on %s', socket.user.name, talk.msg, room_name);
+            //TODO user에서 찾는것이 맞지 않을까?
             var room = roomdb.findRoom(room_name);
-
-            console.log('talk', room_name, talk);
             room.addTalk(talk);
+            socket.broadcast.to(room_name).emit('talk', room_name, talk);
+        });
 
-            socket.broadcast.to(room_name).emit('talk',room_name, talk);
+        socket.on('exit', function(cb) {
+            if (!socket.auth) return sendErr(socket, 'not auth');
+            console.log('%s[%s] exit', socket.user.name, socket.id);
+
+            socket.user.leaveAll(socket);
+            socket.user.setSocketId(null);
+            socket.user = null;
+            socket.auth = false;
+            cb();
         });
 
         socket.on('disconnect', function() {
-            if (socket.user) {
-                socket.user.setSocketId(null);
-                socket.user.leaveAll(socket);
-                console.log(socket.user, ' disconnected');
-            } else {
-                console.log('user disconnected');
-            }
+            console.log('%s disconnected', socket.id);
         });
     });
 };
